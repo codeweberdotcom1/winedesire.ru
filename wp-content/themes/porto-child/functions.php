d4e9bf571e3d601bae70b3b73fcfa8650ff317ce
Denis <web@dgladkov.ru>
2020-06-12 12:36:10 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -520,7 +520,7 @@ function bbloomer_wp_editor_add() {
 add_action( 'product_cat_edit_form_fields', 'bbloomer_wp_editor_edit', 10, 2 );
  
 function bbloomer_wp_editor_edit( $term ) {
-    $second_desc = htmlspecialchars_decode( get_woocommerce_term_meta( $term->term_id, 'seconddesc', true ) );
+    $second_desc = htmlspecialchars_decode( get_term_meta( $term->term_id, 'seconddesc', true ) );
     ?>
     <tr class="form-field">
         <th scope="row" valign="top"><label for="second-desc"><?php echo __( 'Second Description', 'woocommerce' ); ?></label></th>
@@ -566,8 +566,8 @@ add_action( 'woocommerce_after_shop_loop', 'bbloomer_display_wp_editor_content',
 function bbloomer_display_wp_editor_content() {
    if ( is_product_taxonomy() ) {
       $term = get_queried_object();
-      if ( $term && ! empty( get_woocommerce_term_meta( $term->term_id, 'seconddesc', true ) ) ) {
-         echo '<p class="term-description">' . wc_format_content( htmlspecialchars_decode( get_woocommerce_term_meta( $term->term_id, 'seconddesc', true ) ) ) . '</p>';
+      if ( $term && ! empty( get_term_meta( $term->term_id, 'seconddesc', true ) ) ) {
+         echo '<p class="term-description">' . wc_format_content( htmlspecialchars_decode( get_term_meta( $term->term_id, 'seconddesc', true ) ) ) . '</p>';
       }
    }
 }

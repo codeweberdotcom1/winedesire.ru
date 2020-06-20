174f47658815031133d99a39f5cabd9f600461a7
Denis <web@dgladkov.ru>
2020-06-20 14:00:53 +0300

    Update header.php

--- wp-content/themes/porto-child/header.php
+++ wp-content/themes/porto-child/header.php
@@ -9,6 +9,7 @@
 
 	<?php get_template_part( 'head' ); ?>
 </head>
+<a class="cart-contents" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> - <?php echo WC()->cart->get_cart_total(); ?></a>
 <?php
 global $porto_settings;
 $wrapper     = porto_get_wrapper_type();
